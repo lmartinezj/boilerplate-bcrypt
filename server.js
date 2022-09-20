4bded87d3fb9b46c9f72ca8e6d39e38a06217f97
lmartinez <lmartinezj@gmail.com>
2022-09-20 16:18:11 -0500

    tested hash and compare with correct and incorrect passwords

--- server.js
+++ server.js
@@ -8,6 +8,33 @@ const bcrypt  = require('bcrypt');
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
+/**
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+  }
+);
+
+bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res);
+  }
+);
+ 
+bcrypt.compare(someOtherPlaintextPassword, hash, (err, res) => {
+    console.log(res);
+  }
+);
+*/
+
+
+bcrypt.hash('passw0rd!', 13, (err, hash) => {
+    console.log(hash);
+    bcrypt.compare('passw0rd!', hash, (err, res) => {
+      console.log(res); //true
+    });
+    bcrypt.compare(someOtherPlaintextPassword, hash, (err, res) => {
+        console.log(res);
+    });
+});
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
